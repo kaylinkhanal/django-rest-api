f4719edc2541ff02d1746ba3a08c5c29ecfc9f7f
kaylinkhanal <kyalin.khanal@gmail.com>
2019-02-26 15:55:15 +0545

    added model for storing Profile feeed ites and migrated

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -7,3 +7,14 @@ class User(AbstractUser):
 
 class Student(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile= models.ForeignKey('User',on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+
+        return self.status_text
