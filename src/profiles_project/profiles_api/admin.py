10f7a0b5d62b6e9c98fdb8b7aec662ccc694cca5
kaylinkhanal <kyalin.khanal@gmail.com>
2019-05-11 14:19:25 +0545

    Added models

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,6 +1,60 @@
 from django.contrib import admin
-from . import models
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import (
+                     User,
+                     Student,
+                     Faculty,
+                     Subject,
+                     SelectedSubject,
+                     AttendanceRecord,
+                     StudentOfTheYear,
+                     Notice,
+                     FacultyOfTheYear,
+                     Choice,
+                     Question,
+                     Assignment,
+                     GradedAssignment,
+                     ProfileFeedItem
+)
 
-admin.site.register(models.User)
-admin.site.register(models.ProfileFeedItem)
+
+
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'is_student', 'is_teacher', 'password1', 'password2')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'is_student', 'is_teacher', 'password')
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff')
+        })
+    )
+    list_display = ['email', 'username', 'is_student', 'is_teacher']
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+
+admin.site.register(Faculty)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(SelectedSubject)
+admin.site.register(Notice)
+admin.site.register(StudentOfTheYear)
+admin.site.register(FacultyOfTheYear)
+admin.site.register(AttendanceRecord)
+admin.site.register(Choice)
+admin.site.register(Question)
+
+admin.site.register(ProfileFeedItem)
+admin.site.register(Assignment)
+admin.site.register(GradedAssignment)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
